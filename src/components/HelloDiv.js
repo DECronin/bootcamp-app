686c7c1373741ad9642956d7da907240499311ee
Dana Cronin <cronin43224235@gmail.com>
2020-02-03 20:19:07 -0500

    adding activities src folders

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HelloDiv() {
+  return <>
+  <h1>Hello, My Name is Dana!</h1>
+  <ul>
+    <li>I like Animals.</li>
+    <li>I like Food.</li>
+    <li>I loke to Eat, Sleep & Code!</li>
+  </ul>
+  </>;
+}
+
+export default HelloDiv;

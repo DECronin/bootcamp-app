8e3b4b52f7c50218a601cd42cfe22e514cdc3771
Dana Cronin <cronin43224235@gmail.com>
2020-02-03 21:22:50 -0500

    activity 9 -- styling -(havent debugged font color and size yet)

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import React from "react";
 import "../styles/Header.css";
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
+const style = {
+  fontSize: "100px",
+  background: "#ff0000"
+}
 
 function Header() {
   return (
-    <header className="header">
+    <header className="header" style={style}>
       <h1>Welcome</h1>
     </header>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,14 @@ import React from "react";
 import "../styles/Navbar.css";
 
 // By importing the Navbar.css file, it is added to the DOM whenever this component loads
+const styling = {
+  background: "#007700",
+  justifyContent: "flex-end"
+}
 
 function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="navbar" style={styling}>
       <a href="/">Welcome</a>
     </nav>
   );

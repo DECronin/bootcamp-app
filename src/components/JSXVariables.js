4ee5db2312232d925aa14d828f946074b9098d3b
Dana Cronin <cronin43224235@gmail.com>
2020-02-03 20:47:10 -0500

    activity 7 --&-- IT WORKD WITH ARRAYS AND .LENGTH :D

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+const name = ["D", "a", "n", "a"];
+const numLetters = name.length;
+const thoughts = "is convolutedly kool~!"
+
 function JSXVariables() {
   return (
     <div className="main-container">
       <div className="container">
         <div className="jumbotron">
-          <h1>Hi! My name is (insert name here)</h1>
-          <h2>My name has (insert number of letters in name here) letters</h2>
-          <h2>I think React (insert thoughts about React here)</h2>
+          <h1>Hi! My name is {name}</h1>
+          <h2>My name has {numLetters} letters</h2>
+          <h2>I think React {thoughts}</h2>
         </div>
       </div>
     </div>

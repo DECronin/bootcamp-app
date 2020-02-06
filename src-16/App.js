7c33384c86b012f7b49999e529b66080779cd649
Dana Cronin <cronin43224235@gmail.com>
2020-02-05 22:08:23 -0500

    acttivity 16

--- src-16/App.js
+++ src-16/App.js
@@ -1,33 +1,31 @@
 import React from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
+import friends from "./friends.json.js";
 import "./App.css";
 
-function App() {
-  return (
-    <Wrapper>
-      <h1 className="title">Friends List</h1>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
-      />
-      <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
-      />
-      <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
-      />
-    </Wrapper>
-  );
+class App extends React.Component {
+  state = {
+    friends: friends
+  };
+
+  handleFriendship = (id) => {
+    this.setState({ friends:  this.state.friends.filter(friend => friend.id !== id)});
+  };
+
+  render() {
+
+    return (
+      <Wrapper>
+        <h1 className="title">Friends List</h1>
+        <ul>
+          {this.state.friends.map(x =>
+            <FriendCard friend={x} handleFriendship={this.handleFriendship}/>
+          )}          
+        </ul>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
--- src-16/components/FriendCard/index.js
+++ src-16/components/FriendCard/index.js
@@ -2,27 +2,30 @@ import React from "react";
 import "./style.css";
 
 function FriendCard(props) {
+  let x = props.friend;
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+    <li key={x.id}>
+      <div className="card">
+        <div className="img-container">
+          <img alt={x.name} src={x.image} />
+        </div>
+        <div className="content">
+          <ul>
+            <li>
+              <strong>Name:</strong> {x.name}
+            </li>
+            <li>
+              <strong>Occupation:</strong> {x.occupation}
+            </li>
+            <li>
+              <strong>Address:</strong> {x.location}
+            </li>
+          </ul>
+        </div>
+        <span className="remove" onClick={() => props.handleFriendship(x.id)}>𝘅</span>
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Address:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span className="remove">𝘅</span>
-    </div>
-  );
+    </li>
+  )
 }
 
 export default FriendCard;
